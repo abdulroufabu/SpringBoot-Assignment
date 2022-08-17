8b4c5f3fd7e751a79cd3d3bbc70e033760164184
Abdulrouf <Abdulrouf@LAPTOP-ASKR4RV8>
2022-08-17 21:29:49 +0400

    Added test cases for controller

--- Assignment/src/test/java/com/test/statement/AccountStatementApplicationTest.java
+++ Assignment/src/test/java/com/test/statement/AccountStatementApplicationTest.java
@@ -1,5 +1,6 @@
 package com.test.statement;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -14,8 +15,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -49,14 +54,18 @@ public class AccountStatementApplicationTest {
 		searchInput.setFromAmount(200.0);
 		searchInput.setFromAmount(600.0);
 
-		// When
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/accounts")
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.post("/api/v1/accounts")
 				.with(httpBasic("admin", "admin"))
-				.contentType(MediaType.APPLICATION_JSON).with(csrf())
-				.content(objectMapper.writeValueAsString(searchInput))
-				.accept(MediaType.APPLICATION_JSON))
-				.andDo(print()).andExpect(status().isOk());
+				.with(csrf())
+				.accept(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(searchInput))
+				.contentType(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 
+		MockHttpServletResponse response = result.getResponse();
+
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
 	}
 
 	@Test
@@ -72,7 +81,8 @@ public class AccountStatementApplicationTest {
 
 		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/accounts")
 				.with(httpBasic("admin", "admin"))
-				.contentType(MediaType.APPLICATION_JSON).with(csrf())
+				.with(csrf())
+				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(searchInput)).accept(MediaType.APPLICATION_JSON))
 				.andDo(print()).andExpect(status().isBadRequest());
 	}
